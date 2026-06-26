b1f5a09dc8fbf33f9eae313b8ee75a8626aa2f41
Randall Quesada <randallquesadaa@gmail.com>
2026-06-26 12:20:37 -0600

    Pantheon settings

--- web/sites/default/settings.pantheon.php
+++ web/sites/default/settings.pantheon.php
@@ -3,42 +3,50 @@
 /**
  * Configuracion de Drupal para entornos Pantheon.
  *
- * Este archivo es incluido por settings.php solo cuando
- * $_ENV['PANTHEON_ENVIRONMENT'] esta definido.
+ * Incluido por settings.php cuando $_ENV['PANTHEON_ENVIRONMENT'] existe.
  */
 
-// Base de datos: Pantheon expone las credenciales via $_ENV.
-$databases['default']['default'] = [
-  'driver'    => 'mysql',
-  'database'  => $_ENV['DB_NAME'],
-  'username'  => $_ENV['DB_USER'],
-  'password'  => $_ENV['DB_PASSWORD'],
-  'host'      => $_ENV['DB_HOST'],
-  'port'      => $_ENV['DB_PORT'],
-  'prefix'    => '',
-  'collation' => 'utf8mb4_general_ci',
-];
+// Base de datos via variables de entorno de Pantheon.
+if (isset($_ENV['DB_HOST'])) {
+  $databases['default']['default'] = [
+    'driver'    => 'mysql',
+    'database'  => $_ENV['DB_NAME'],
+    'username'  => $_ENV['DB_USER'],
+    'password'  => $_ENV['DB_PASSWORD'],
+    'host'      => $_ENV['DB_HOST'],
+    'port'      => $_ENV['DB_PORT'],
+    'prefix'    => '',
+    'collation' => 'utf8mb4_general_ci',
+  ];
+}
 
-// Hash salt unico por sitio, provisto por Pantheon.
-$settings['hash_salt'] = $_ENV['DRUPAL_HASH_SALT'];
+// Hash salt provisto por Pantheon.
+if (isset($_ENV['DRUPAL_HASH_SALT'])) {
+  $settings['hash_salt'] = $_ENV['DRUPAL_HASH_SALT'];
+}
 
-// Hosts de confianza para todos los subdominios de Pantheon.
+// Hosts de confianza para subdominios de Pantheon.
 $settings['trusted_host_patterns'] = [
   '^.+\.pantheonsite\.io$',
   '^.+\.pantheon\.io$',
   '^.+\.getpantheon\.com$',
 ];
 
-// Rutas de archivos dentro del servidor de Pantheon.
-$settings['file_public_path']  = 'sites/default/files';
-$settings['file_private_path'] = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/files/private';
-$settings['file_temp_path']    = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/tmp';
+// Rutas de archivos.
+$settings['file_public_path'] = 'sites/default/files';
+if (isset($_ENV['PANTHEON_BINDING'])) {
+  $settings['file_private_path'] = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/files/private';
+  $settings['file_temp_path']    = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/tmp';
+}
+else {
+  $settings['file_private_path'] = 'sites/default/files/private';
+  $settings['file_temp_path']    = '/tmp';
+}
 
-// Deshabilitar verificacion de permisos de archivos (Pantheon los maneja).
+// Pantheon maneja los permisos de archivos.
 $settings['skip_permissions_hardening'] = TRUE;
 
-// Configuracion de cache con Redis si esta disponible en el plan.
-// Descomentar si tu plan de Pantheon incluye Redis.
+// Configuracion de Redis (descomentar si tu plan incluye Redis).
 // if (isset($_ENV['CACHE_HOST'])) {
 //   $settings['redis.connection']['interface'] = 'PhpRedis';
 //   $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
