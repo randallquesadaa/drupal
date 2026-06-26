cea9ae8f1757e53fcd8dc86ef443e8a4c3a5ba0c
Randall Quesada <randallquesadaa@gmail.com>
2026-06-26 12:22:37 -0600

    Pantheon deploy ready

--- web/sites/default/settings.pantheon.php
+++ web/sites/default/settings.pantheon.php
@@ -32,25 +32,5 @@ $settings['trusted_host_patterns'] = [
   '^.+\.getpantheon\.com$',
 ];
 
-// Rutas de archivos.
-$settings['file_public_path'] = 'sites/default/files';
-if (isset($_ENV['PANTHEON_BINDING'])) {
-  $settings['file_private_path'] = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/files/private';
-  $settings['file_temp_path']    = '/srv/bindings/' . $_ENV['PANTHEON_BINDING'] . '/tmp';
-}
-else {
-  $settings['file_private_path'] = 'sites/default/files/private';
-  $settings['file_temp_path']    = '/tmp';
-}
-
 // Pantheon maneja los permisos de archivos.
 $settings['skip_permissions_hardening'] = TRUE;
-
-// Configuracion de Redis (descomentar si tu plan incluye Redis).
-// if (isset($_ENV['CACHE_HOST'])) {
-//   $settings['redis.connection']['interface'] = 'PhpRedis';
-//   $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
-//   $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
-//   $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
-//   $settings['cache']['default'] = 'cache.backend.redis';
-// }
